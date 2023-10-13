3b5b9e990017b25bbc242ef683ac6a46ea27725f
ibrahimdmrck@gmail.com <ibrahimdmrck@gmail.com>
2023-10-13 21:00:40 +0300

    Add project files.

---
+++ Core/Entities/Concrete/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities.Concrete
+{
+    public class User:IEntity
+    {
+        public int Id { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Email { get; set; }
+        public required byte[] PasswordHash { get; set; }
+        public required byte[] PasswordSalt { get; set; }
+        public bool Status { get; set; }
+    }
+}
