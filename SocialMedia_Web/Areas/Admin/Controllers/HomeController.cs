0300ef476a9d82971ac40848310ad06ab06859ed
ibrahimdmrck@gmail.com <ibrahimdmrck@gmail.com>
2024-03-17 17:37:40 +0300

    admin paneli ana sayfasında paylaşımlar datatable üzerinde listelendi

--- SocialMedia_Web/Areas/Admin/Controllers/HomeController.cs
+++ SocialMedia_Web/Areas/Admin/Controllers/HomeController.cs
@@ -1,15 +1,32 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SocialMedia_Web.Models;
 
 namespace SocialMedia_Web.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class HomeController : Controller
     {
-        [Authorize(Roles = "admin")]
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public HomeController(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Authorize(Roles = "admin")]
+        public async Task<IActionResult> Index()
+        {
+            var responseMessage = await _httpClientFactory.CreateClient().GetAsync("http://localhost:65525/api/Articles/getarticlewithdetails");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonResponse = await responseMessage.Content.ReadAsStringAsync();
+                var apiDataResponse = JsonConvert.DeserializeObject<ApiListDataResponse<ArticleDetailDto>>(jsonResponse);
+
+                return apiDataResponse.Success ? View(apiDataResponse.Data) : View("Veri gelmiyor");
+            }
+            return View("Veri gelmiyor");
         }
     }
 }
