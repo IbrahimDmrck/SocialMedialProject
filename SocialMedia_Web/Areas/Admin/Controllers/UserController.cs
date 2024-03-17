ee2fbac6c17943e1c1e882d412398d52178888b1
ibrahimdmrck@gmail.com <ibrahimdmrck@gmail.com>
2024-03-17 18:54:18 +0300

    admin panelinde kullanıcılar listelendi

---
+++ SocialMedia_Web/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SocialMedia_Web.Models;
+
+namespace SocialMedia_Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public UserController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Authorize(Roles = "admin")]
+        public async Task<IActionResult> Index()
+        {
+            var responseMessage = await _httpClientFactory.CreateClient().GetAsync("http://localhost:65525/api/Users/getalldto");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonResponse = await responseMessage.Content.ReadAsStringAsync();
+                var apiDataResponse = JsonConvert.DeserializeObject<ApiListDataResponse<UserDto>>(jsonResponse);
+
+                return apiDataResponse.Success ? View(apiDataResponse.Data) : View("Veri gelmiyor");
+            }
+            return View("Veri gelmiyor");
+        }
+    }
+}
--- Web_Api/Controllers/UsersController.cs
+++ Web_Api/Controllers/UsersController.cs
@@ -27,6 +27,14 @@ namespace WebAPI.Controllers
             return result.Success ? Ok(result) : BadRequest(result);
         }
 
+        [HttpGet("getalldto")]
+        public IActionResult GetAllDto()
+        {
+
+            IDataResult<List<UserDto>> result = _userService.GetAllDto();
+            return result.Success ? Ok(result) : BadRequest(result);
+        }
+
         [HttpGet("getbyid")]
         public IActionResult GetById(int id)
         {
