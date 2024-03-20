3e9baf86a2307049564b9fdc7b39fd6625b3e9d9
ibrahimdmrck@gmail.com <ibrahimdmrck@gmail.com>
2024-03-21 00:46:22 +0300

    topic ekleme işlemi yapıldı

--- SocialMedia_Web/Areas/Admin/Controllers/TopicController.cs
+++ SocialMedia_Web/Areas/Admin/Controllers/TopicController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using SocialMedia_Web.Models;
 using System.Net.Http.Headers;
+using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace SocialMedia_Web.Areas.Admin.Controllers
 {
@@ -49,6 +51,61 @@ namespace SocialMedia_Web.Areas.Admin.Controllers
                     Url = "/Admin/Topic/Index"
                 };
                 return Json(response);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        [Authorize(Roles = "admin")]
+        [HttpGet]
+        public async Task<IActionResult> GetUpdateTopic(int id)
+        {
+
+            var responseMessage = await _httpClientFactory.CreateClient().GetAsync("http://localhost:65525/api/Topics/getbyid?id="+id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonResponse1 = await responseMessage.Content.ReadAsStringAsync();
+                var apiDataResponse = JsonConvert.DeserializeObject<ApiDataResponse<Topics>>(jsonResponse1);
+
+
+                return View(apiDataResponse.Data);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        [Authorize(Roles = "admin")]
+        [HttpPut]
+        public async Task<IActionResult> UpdateTopic(Topics topics)
+        {
+            var jsonTopic = JsonConvert.SerializeObject(topics);
+            var content = new StringContent(jsonTopic, Encoding.UTF8, "application/json");
+            var token = HttpContext.Session.GetString("Token");
+            _httpClientFactory.CreateClient().DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var responseMessage = await _httpClientFactory.CreateClient().PutAsync("http://localhost:65525/api/Topics/update", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var responseContent = await responseMessage.Content.ReadAsStringAsync();
+                var data = JsonConvert.DeserializeObject<ApiDataResponse<Topics>>(responseContent);
+                var response = new
+                {
+                    Message = data.Message
+                };
+                return Json(response);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        [Authorize(Roles = "admin")]
+        [HttpPost]
+        public async Task<IActionResult> AddTopic(Topics topics)
+        {
+            var jsonTopic = JsonConvert.SerializeObject(topics);
+            var content = new StringContent(jsonTopic, Encoding.UTF8, "application/json");
+            var token = HttpContext.Session.GetString("Token");
+            _httpClientFactory.CreateClient().DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var responseMessage = await _httpClientFactory.CreateClient().PostAsync("http://localhost:65525/api/Topics/add",content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Topic");
             }
             return RedirectToAction("Index", "Home");
         }
